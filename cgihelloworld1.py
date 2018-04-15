aadcab8da4f84feacbd34598f21f93235d531312
biohack5079 <biohack5079@gmail.com>
2018-04-16 03:09:30 +0900

    cgihelloworld1.py

---
+++ cgihelloworld1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+print("Content-Type: text/html;")
+print("")
+print("<!DOCTYPE html>")
+print("<html lang='ja'>")
+print("<head>")
+print("    <meta charset='utf-8'>")
+print("    <title>hello world.</title>")
+print("</head>")
+print("<body>")
+print("    <h1>hello world.</h1>")
+print("</body>")
+print("</html>")
---
+++ cgihelloworld2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+# enable debugging
+import cgitb
+cgitb.enable()
+
+print("Content-Type: text/plain;charset=utf-8")
+print()
+
+print("Hello World!")
